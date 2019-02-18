068ec035e286704c45b93ff6a445ad3bb1d690a7
fabricezerrouki <43605041+fabricezerrouki@users.noreply.github.com>
2019-02-18 16:51:07 +0100

    Update service.js

--- database/service.js
+++ database/service.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const serviceSchema = new Schema({
     name: { type: String },
-    location: { type: String }
+    location: { type: String },
+    people: { type: Schema.Types.ObjectId, ref: 'people' }
 },{ versionKey: false })
 
 module.exports = mongoose.model('service', serviceSchema);
