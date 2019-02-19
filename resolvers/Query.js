cdc89a41cf2c837560842b5fac1f18a0e8def5a1
ric0 <e.cerroni@gmail.com>
2019-02-19 18:55:34 +0100

    Update people resolver
    Build forehand all fields graphql schema is looking for

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,7 +1,17 @@
 const People = require('../database/people');
 const Service = require('../database/service');
 const queries = {
-    People: () => People.find({}),
+    People: async () => {
+      const people = await People.find({}).populate('service').exec()
+      return people.map(person => ({
+        ...person._doc,
+        id: person._doc._id,
+        service: {
+          ...person._doc.service._doc,
+          id: person._doc.service._doc._id,
+        },
+      }))
+    },
     PeopleByName: (root,args,context,info) => People.find({lastName: args.lastName}),
     PeopleByID: (root,args,context,info) => People.find({_id: args.id}),
     PeopleByXid: (root,args,context,info) => People.find({Xid: args.Xid}),
